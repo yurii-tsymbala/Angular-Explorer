52c80f99520b0ed4d5f885af4044d30cd6907944
Yurii <zumbala2013@gmail.com>
2024-01-01 23:23:14 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { Task } from './model/task';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-educ';
+  newTask: string = '';
+  tasks: Task[] = [];
+  constructor() {
+    this.tasks = this.fetchTasks();
+  }
+
+  onChange() {
+    console.log(this.newTask);
+  }
+
+  onCreate() {
+    this.tasks.push(new Task(this.newTask));
+    this.newTask = '';
+  }
+
+  fetchTasks(): Task[] {
+    return [
+      new Task('buy a car'),
+      new Task('prepare the food'),
+      new Task('build a house'),
+    ];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TaskInputComponent } from './component/task-input/task-input.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskInputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
